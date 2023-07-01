f010702199a8d3d3e2fc16b4b95ec6a7a8047c03
Abdul Rahman Hamidy <arahmancsd@gmail.com>
2023-07-01 19:18:40 +0100

    Maui Test project
    RequestedThemeChanged not functioning properly

---
+++ MauiTestApp1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace MauiTestApp1;
+
+public partial class App : Application
+{
+    public App()
+	{
+		InitializeComponent();
+
+        Current.RequestedThemeChanged += Current_RequestedThemeChanged;
+
+        MainPage = new AppShell();
+    }
+
+    private void Current_RequestedThemeChanged(object sender, AppThemeChangedEventArgs e)
+    {
+        // Don't handle events fired for old application instances
+        // and cleanup the old instance's event handler
+        if (Current != this && Current is App app)
+        {
+            Current.RequestedThemeChanged -= Current_RequestedThemeChanged;
+            return;
+        }
+
+        Console.WriteLine($"{e.RequestedTheme}");
+
+        UserAppTheme = e.RequestedTheme;
+    }
+}
+
